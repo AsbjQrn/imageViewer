9f936a3fac408d6d87dbe0f01d76ee13fe0265cc
askou <askou@dxc.com>
2019-11-13 16:45:19 +0100

    MediaWalker

---
+++ src/main/java/dk/asbjoern/foto/mediasources/MediaWalker.java
@@ -0,0 +1,15 @@
+package dk.asbjoern.foto.mediasources;
+
+
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+
+@Component
+public class MediaWalker {
+
+
+    Files.walk(sourcePath).forEach(path -> {}
+
+
+}
